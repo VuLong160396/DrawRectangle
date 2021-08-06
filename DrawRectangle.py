db2d251e505afdcf9e100f364f077830d5a6b685
7-longvv <7-longvv@phenikaa.local>
2021-08-06 20:55:07 +0700

    them file DrawRectangle

---
+++ DrawRectangle.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+l = 200
+w = 100
+t.speed(1)
+#vẽ cạnh đầu tiên
+t.fd(l)
+t.lt(90)
+#vẽ cạnh thứ 2
+t.fd(w)
+t.lt(90)
+#vẽ cạnh đầu tiên
+t.fd(l)
+t.lt(90)
+#vẽ cạnh thứ 2
+t.fd(w)
+t.lt(90)
